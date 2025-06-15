c92cc996bd42a3f706af2898f04774c894ea800f
Tazwar Saif Chowdhury <160275170+tazwarsaif@users.noreply.github.com>
2025-06-15 16:16:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const siteURL = "https://my-portfolio-ftoh.onrender.com/"; // Change to your site URL
-    const iframe = document.getElementById("websiteFrame");
+    let siteURL = "https://my-portfolio-ftoh.onrender.com/"; // Change to your site URL
+    let iframe = document.getElementById("websiteFrame");
 
     function loadWebsite() {
         iframe.src = siteURL + "?t=" + new Date().getTime(); // Add timestamp to avoid caching issues
         iframe.style.display = "block";
         document.getElementById("message").innerText = "Website refreshed!";
+    }
+
+    // Load immediately and repeat every 10 seconds
+    loadWebsite();
+    setInterval(loadWebsite, 20000); // Reload every 10 seconds
+
+    let siteURL = "https://ai-note-summarizer-tfmx.onrender.com/"; // Change to your site URL
+    let iframe = document.getElementById("websiteFrame2");
+
+    function loadWebsite() {
+        iframe.src = siteURL + "?t=" + new Date().getTime(); // Add timestamp to avoid caching issues
+        iframe.style.display = "block";
+        document.getElementById("message2").innerText = "Website refreshed!";
     }
 
     // Load immediately and repeat every 10 seconds
